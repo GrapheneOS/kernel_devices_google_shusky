b5ac6daf44e4f2d9019b9291fc4bc2dd506d5e05
Robin Peng <robinpeng@google.com>
2022-08-02 17:04:54 +0800

    Initial bring up for shusky kernel device
    bring up from
    private/google-modules/soc/gs: 8af864e397fcf874e3de5f197162d8aacd69253c
    
    temporary use symlink for below files:
    1) shusky_gki.fragment (ripcurrent_gki.fragment)
    2) shusky_modified_gki.fragment (ripcurrent_modified_gki.fragment)
    3) vendor_boot_modules.shusky (vendor_boot_modules.zuma)
    
    Bug: 241064049
    Signed-off-by: Robin Peng <robinpeng@google.com>
    Change-Id: I775b6733ee2c8e10b3306f0ef3e3eb94252c2eaf

---
+++ build_shusky.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+function exit_if_error {
+  if [ $1 -ne 0 ]; then
+    echo "ERROR: $2: retval=$1" >&2
+    exit $1
+  fi
+}
+
+export LTO=${LTO:-thin}
+export BUILD_CONFIG=private/devices/google/shusky/build.config.shusky
+
+if [ -z "${GKI_PREBUILTS_DIR}" ]; then
+  CORE_KERNEL_FRAGMENT_DEFCONFIG=../boot/dts/google/devices/shusky/../shusky_modified_gki.fragment \
+  GKI_BUILD_CONFIG=private/google-modules/soc/gs/build.config.zuma.gki \
+  build/build.sh "$@"
+else
+  build/build.sh "$@"
+fi
+
+exit_if_error $? "Failed to create mixed build"
