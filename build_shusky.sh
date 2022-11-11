6e750341543efe16c6498ab4015cd7932752fc48
Will McVicker <willmcvicker@google.com>
2022-11-11 02:35:55 -0800

    Clean up the configs and script
    * Clean up the path definitions to be more clear on what is what.
    * Convert the modified GKI defconfig fragment to
      a GKI_BUILD_CONFIG_FRAGMENT so that we can directly use the
      build.config.gki.arch64 GKI_BUILD_CONFIG.
    * Set KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1 for build_shusky.sh.
    
    Change-Id: Ibd8e98f846deff3f45ae43071b5b21f9ed6b48b4
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- build_shusky.sh
+++ build_shusky.sh
@@ -8,13 +8,21 @@ function exit_if_error {
   fi
 }
 
+export GKI_KERNEL_DIR=${GKI_KERNEL_DIR:-"aosp-staging"}
+export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
 export LTO=${LTO:-thin}
 export BUILD_CONFIG=private/devices/google/shusky/build.config.shusky
 
+# TODO(b/239987494): Remove this when the core GKI fragment is removed.
+# Since we can't have two build config fragments, any alternative use
+# of GKI_BUILD_CONFIG_FRAGMENT must include CORE_GKI_FRAGMENT,
+# i.e. `source ${CORE_GKI_FRAGMENT}`.
+export CORE_GKI_FRAGMENT=private/devices/google/shusky/build.config.shusky.gki.fragment
+
 if [ -z "${GKI_PREBUILTS_DIR}" ]; then
-  CORE_KERNEL_FRAGMENT_DEFCONFIG=../boot/dts/google/devices/shusky/../../shusky_modified_gki.fragment \
-  GKI_BUILD_CONFIG=private/google-modules/soc/gs/build.config.zuma.gki \
-  build/build.sh "$@"
+  GKI_BUILD_CONFIG_FRAGMENT=${GKI_BUILD_CONFIG_FRAGMENT:-${CORE_GKI_FRAGMENT}} \
+  GKI_BUILD_CONFIG="${GKI_KERNEL_DIR}/build.config.gki.aarch64" \
+    build/build.sh "$@"
 else
   build/build.sh "$@"
 fi
