55ec2233ef478e20aefaa5bf2177717dbd42527c
Chris Lu <luchris@google.com>
2023-03-14 16:27:32 +0800

    Revert "display: hk3: use 10Hz for 120Hz panel idle by default"
    This reverts commit 7bf8fef25b6d8493037011f45c1f7f1c715c8451.
    Restore the default panel idle to 1Hz to save power.
    
    Bug: 256662691
    Test: check log
    Change-Id: I89a687a99a5e4054cd9f54424d43151a05050723
    Signed-off-by: Chris Lu <luchris@google.com>

--- display/panel-google-hk3.c
+++ display/panel-google-hk3.c
@@ -465,7 +465,6 @@ static inline bool is_auto_mode_allowed(struct exynos_panel *ctx)
 	return ctx->panel_idle_enabled;
 }
 
-#define HK3_DEFAULT_MIN_VREFRESH 10
 static u32 hk3_get_min_idle_vrefresh(struct exynos_panel *ctx,
 				     const struct exynos_panel_mode *pmode)
 {
@@ -475,9 +474,7 @@ static u32 hk3_get_min_idle_vrefresh(struct exynos_panel *ctx,
 	if ((min_idle_vrefresh < 0) || !is_auto_mode_allowed(ctx))
 		return 0;
 
-	if (!min_idle_vrefresh)
-		min_idle_vrefresh = HK3_DEFAULT_MIN_VREFRESH;
-	else if (min_idle_vrefresh == 1)
+	if (min_idle_vrefresh <= 1)
 		min_idle_vrefresh = 1;
 	else if (min_idle_vrefresh <= 10)
 		min_idle_vrefresh = 10;
