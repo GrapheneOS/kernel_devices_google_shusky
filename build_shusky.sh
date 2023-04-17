bc310b84fc61105c765548fca22d6656da2e01c4
Lucas Wei <lucaswei@google.com>
2023-04-17 08:15:33 +0000

    shusky: Re-enable ARM64_SVE
    ARM64_SVE was disabled via a GKI_BUILD_CONFIG_FRAGMENT. We can fully
    remove build.config.shusky.gki.fragment now that we ar re-enabling SVE.
    
    Bug: 232434016
    Bug: 239987494
    Change-Id: I65667c7b6c990b551af3e9ff46c1f257bad16646
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- build_shusky.sh
+++ build_shusky.sh
@@ -34,14 +34,8 @@ export LTO=${LTO:-thin}
 export BUILD_CONFIG=private/devices/google/shusky/build.config.shusky
 export VENDOR_DLKM_ETC_FILES=private/devices/google/shusky/insmod_cfg/*
 
-# TODO(b/239987494): Remove this when the core GKI fragment is removed.
-# Since we can't have two build config fragments, any alternative use
-# of GKI_BUILD_CONFIG_FRAGMENT must include CORE_GKI_FRAGMENT,
-# i.e. `source ${CORE_GKI_FRAGMENT}`.
-export CORE_GKI_FRAGMENT=private/devices/google/shusky/build.config.shusky.gki.fragment
-
 if [ -z "${GKI_PREBUILTS_DIR}" ]; then
-  GKI_BUILD_CONFIG_FRAGMENT=${GKI_BUILD_CONFIG_FRAGMENT:-${CORE_GKI_FRAGMENT}} \
+  GKI_BUILD_CONFIG_FRAGMENT=${GKI_BUILD_CONFIG_FRAGMENT:-""} \
   GKI_BUILD_CONFIG="${GKI_KERNEL_DIR}/build.config.gki.aarch64" \
     build/build.sh "$@"
 else
