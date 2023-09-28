cf0dc8ca899b7e5775679db6ce332093ab548c64
Robin Peng <robinpeng@google.com>
2023-09-28 02:27:26 +0000

    build_<DEVICE>.sh: fix string parsing issue
    exclude the comment lines to avoid incorrect parsing results
    
    Bug: 289768840
    Change-Id: I377b4ed12e09a73353d05b6e2e4a5d30f644548f
    Signed-off-by: Robin Peng <robinpeng@google.com>

--- build_shusky.sh
+++ build_shusky.sh
@@ -14,17 +14,19 @@ if [ "${BUILD_AOSP_KERNEL}" = "1" -a "${BUILD_STAGING_KERNEL}" = "1" ]; then
 fi
 
 parameters=
-GKI_BUILD_ID=`sed -n 's/.*gki_prebuilts=\([0-9]\+\).*/\1/p' private/google-modules/soc/gs/device.bazelrc`
-USE_GKI=`grep "build --config=download_gki" private/google-modules/soc/gs/device.bazelrc`
-NO_USE_GKI=`grep "build --config=no_download_gki" private/google-modules/soc/gs/device.bazelrc`
-USE_AOSP_OR_AOSP_STAGING=`grep "build --config=use_source_tree_aosp" private/google-modules/soc/gs/device.bazelrc`
-if [ "${BUILD_AOSP_KERNEL}" = "1" ]; then
+bazelrc_file="private/google-modules/soc/gs/device.bazelrc"
+GKI_BUILD_ID=`sed -n 's/.*gki_prebuilts=\([0-9]\+\).*/\1/p' $bazelrc_file`
+USE_GKI=`grep "^build --config=download_gki" $bazelrc_file`
+NO_USE_GKI=`grep "^build --config=no_download_gki" $bazelrc_file`
+USE_AOSP=`grep "^build --config=use_source_tree_aosp$" $bazelrc_file`
+USE_AOSP_STAGING=`grep "^build --config=use_source_tree_aosp_staging$" $bazelrc_file`
+if [ "${BUILD_AOSP_KERNEL}" = "1" ] || [ -n "${USE_AOSP}" ]; then
   echo -e "\nBuilding with core-kernel generated from source tree aosp/\n"
   parameters="--config=use_source_tree_aosp --config=no_download_gki"
-elif [ "${BUILD_STAGING_KERNEL}" = "1" ]; then
+elif [ "${BUILD_STAGING_KERNEL}" = "1" ] || [ -n "${USE_AOSP_STAGING}" ]; then
   echo -e "\nBuilding with core-kernel generated from source tree aosp-staging/\n"
   parameters="--config=use_source_tree_aosp_staging --config=no_download_gki"
-elif [ -n "${GKI_BUILD_ID}" ] && [ -n "${USE_GKI}" ] && [ -z "${NO_USE_GKI}" ] && [ -z "${USE_AOSP_OR_AOSP_STAGING}" ]; then
+elif [ -n "${GKI_BUILD_ID}" ] && [ -n "${USE_GKI}" ] && [ -z "${NO_USE_GKI}" ] && [ -z "${USE_AOSP}" ] && [ -z "${USE_AOSP_STAGING}" ]; then
   echo -e "\nBuilding with GKI prebuilts from ab/$GKI_BUILD_ID - kernel_aarch64\n"
 else
   echo -e "\nPlease check private/google-modules/soc/gs/device.bazelrc"
